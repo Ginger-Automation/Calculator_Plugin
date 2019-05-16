6d7a1aa884aacdcb838e84d330bee016c6a71917
Meni Kadosh <MENIK@amdocs.com>
2019-05-16 19:49:31 +0300

    Pushiing Solution created

---
+++ CalculatorPlugin/CalculatorPlugin/CalculatorService.cs
@@ -0,0 +1,42 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorPlugin
+{
+    [GingerService("CalculatorExecuter", "Do calculation via Ginger")]
+    public class CalculatorService
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="GA"></param>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        [GingerAction("Sum", "Do Sum")]
+        public void ExecuteSum(IGingerAction GA, string num1, string num2)
+        {
+            int num1Int = int.Parse(num1);
+            int num2Int = int.Parse(num2);
+            GA.AddOutput("Result", num1Int + num2Int);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="GA"></param>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        [GingerAction("Minus", "Do Minus")]
+        public void ExecuteMinus(IGingerAction GA, string num1, string num2)
+        {
+            int num1Int = int.Parse(num1);
+            int num2Int = int.Parse(num2);
+            GA.AddOutput("Result", num1Int - num2Int);
+
+        }
+    }
+}
---
+++ CalculatorPlugin/CalculatorPlugin/Program.cs
@@ -0,0 +1,19 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using System;
+
+namespace CalculatorPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Ginger calculator Plugin");
+
+            using (GingerNodeStarter gingerNodeStarter = new GingerNodeStarter())
+            {
+                gingerNodeStarter.StartNode("calculator Service", new CalculatorService());
+                gingerNodeStarter.Listen();
+            }
+        }
+    }
+}
